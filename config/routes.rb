fcd7a63dcec8bcb6eda562db7ec3272b7acc6d15
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-08-21 04:55:39 +0000

    Add モデルコントローラーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'books/index'
+  get 'books/show'
+  root to: "homes#top"
+  get 'homes/about' => "homes#about", as:"about"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
