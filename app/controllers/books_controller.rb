df95ca109b7189df0514e7df60f9e796d8ea9774
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-08-27 15:32:36 +0000

    Update インデックスページの更新

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,23 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "successfully 新規追加に成功しました"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
   def show
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
   end
 end
---
+++ db/migrate/20220827133552_add_columns_to_users.rb
@@ -0,0 +1,6 @@
+class AddColumnsToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :introduction, :text
+  end
+
+end
