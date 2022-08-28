de1e01876b891efa7ee795811405974776865130
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-08-28 14:34:40 +0000

    Update book editページの更新

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,6 +20,10 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body, :user_id)
