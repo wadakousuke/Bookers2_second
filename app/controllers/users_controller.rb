ec4c6cc0a8d56693307d8ee144813b7b69e26efd
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-08-29 14:52:15 +0000

    Add ユーザー情報編集機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books.all
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :body)
+  end
 end
