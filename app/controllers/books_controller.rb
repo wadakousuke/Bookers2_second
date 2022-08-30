6e969b47b574fb4584171571bb0dd105028e93e0
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-08-30 14:44:28 +0000

    Add ブック編集削除機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,9 +19,19 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
 
   def edit
     @book = Book.find(params[:id])
+  end
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :body)
+    params.require(:user).permit(:name, :introduction)
   end
 end
