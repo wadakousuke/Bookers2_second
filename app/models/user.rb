083f7e598cbc8a17c1fe218d56b03835c48793ae
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-09-01 14:14:57 +0000

    Update ユーザー編集画面の更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,15 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
-  def get_profile_image(weight, height)
-    unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-   profile_image.variant(resize_to_limit: [weight, height]).processed
+
+ def get_profile_image(weight, height)
+  unless profile_image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   end
+   profile_image.variant(resize_to_limit: [weight, height]).processed
+ end
+
+ validates :name , presence:true, uniqueness: true, length: { in: 2..20 }
+ validates :introduction , length: { maximum: 50 }
 end
