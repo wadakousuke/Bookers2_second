faabadb7bec16cdffc76f3a57cf05848fd27d5a5
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-09-02 14:51:36 +0000

    Update バリデーションの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,27 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @book = Book.new
 
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books.all
+    @book = Book.new
   end
   def edit
     @user = User.find(params[:id])
   end
+
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id)
+    @user = User.find(params[:id])
+    if@user.update(user_params)
+      flash[:notice] = "successfully 更新に成功しました"
+      redirect_to user_path(current_user.id)
+    else
+       render "edit"
+    end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many:books, dependent: :destroy
 
   has_one_attached :profile_image
+  validates :name, presence: true,length: { in: 2..20 } , uniqueness: true
+  validates :introduction, presence: true, length: { maximum: 50 }
 
 
  def get_profile_image(weight, height)
